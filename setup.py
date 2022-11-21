661bd316a13005700b3ad9e6c9e5aa35b30d9350
philipperemy <premy.enseirb@gmail.com>
2022-11-21 14:17:59 +0700

    bybit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bitpy',
-    version='1.5',
+    version='1.6',
     description='BitPy',
     author='Philippe Remy',
     install_requires=[
