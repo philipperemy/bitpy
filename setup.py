3a6bc07e7e20549e0bb3a2bfd73498913692d195
philipperemy <premy.enseirb@gmail.com>
2023-03-17 10:41:22 +0700

    1.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bitpy',
-    version='1.16',
+    version='1.17',
     description='BitPy',
     author='Philippe Remy',
     install_requires=[
